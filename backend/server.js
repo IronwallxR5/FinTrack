677949720da5c8290a57f03aa1a974e1202821b0
IronwallxR5 <rathipadamr5@gmail.com>
2026-03-05 20:03:30 +0530

    fixed error

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,6 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 app.use(cors({ origin: process.env.FRONTEND_URL || "*", credentials: true }));
-);
 app.use(express.json());
 
 app.use(
